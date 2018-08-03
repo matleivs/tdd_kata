1345e5828c4091b0911040082687b0be89dd3c62
nikbin@itemis.de <nikbin@itemis.de>
2018-08-03 16:42:39 +0200

    RED - add first test case to introduce StringCalculator class.

---
+++ TDD01_StringCalculator/src/StringCalculatorTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+/**
+ * #1 Kata “String Calculator”:
+ * <p>
+ * Erstellt eine Funktion, die einen String entgegennimmt und das Ergebnis der darin erhaltenen Rechnung zurückgibt.
+ * Unterstützt werden sollen die vier Grundrechenarten (+,-,/,*) für Integerwerte.
+ * Die Signatur sähe so aus: Number calculate(String expression)
+ */
+public class StringCalculatorTest {
+
+    @Test
+    public void test() {
+        new StringCalculator();
+    }
+}
