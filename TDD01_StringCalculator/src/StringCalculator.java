3a64731e9308bd613424368febef61d2392cba7f
nikbin@itemis.de <nikbin@itemis.de>
2018-08-31 12:04:48 +0200

    GREEN - test_input_negative1_return_negative1

--- TDD01_StringCalculator/src/StringCalculator.java
+++ TDD01_StringCalculator/src/StringCalculator.java
@@ -4,6 +4,9 @@ public class StringCalculator {
     public int calculate(String s) {
         if (s.equals("0"))
             return 0;
-        return 1;
+        else if (s.equals("1"))
+            return 1;
+        else
+            return -1;
     }
 }
