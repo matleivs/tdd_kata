4404cf620589ce246fc1c5a5ce88c420a53ce495
nikbin@itemis.de <nikbin@itemis.de>
2018-11-02 15:26:10 +0100

    REFACTOR - consistent naming for test methods

--- TDD01_StringCalculator/src/StringCalculatorTest.java
+++ TDD01_StringCalculator/src/StringCalculatorTest.java
@@ -21,63 +21,63 @@ public class StringCalculatorTest {
 
 
     @Test(expected = IllegalArgumentException.class)
-    public void test_invalid_input_throw_illegalArgumentException() {
+    public void input_letters_throw_IllegalArgumentException() {
         new StringCalculator().calculate("abc");
     }
 
 
     @Test(expected = IllegalArgumentException.class)
-    public void test_input_secondOperandIsMissing_throw_illegalArgumentException() {
+    public void input_second_operand_is_missing_throw_IllegalArgumentException() {
         new StringCalculator().calculate("1+");
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void test_input_firstOperandIsMissing_throw_illegalArgumentException() {
+    public void input_first_operand_is_missing_throw_IllegalArgumentException() {
         new StringCalculator().calculate("+1");
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void test_input_firstAndSecondOperandsAreMissing_throw_illegalArgumentException() {
+    public void input_first_and_second_operands_are_missing_throw_IllegalArgumentException() {
         new StringCalculator().calculate("+");
     }
 
     @Test
-    public void test_input_zero_return_0() {
+    public void input_0_return_0() {
         test_calculate("0", 0);
     }
 
     @Test
-    public void test_input_1_return_1() {
+    public void input_1_return_1() {
         test_calculate("1", 1);
     }
 
     @Test
-    public void test_input_negative1_return_negative1() {
+    public void input_negative1_return_negative1() {
         test_calculate("-1", -1);
     }
 
     @Test
-    public void test_input_2digits_return_2digits() {
+    public void input_two_digits_return_two_digits() {
         test_calculate("12", 12);
     }
 
     @Test
-    public void test_input_0plus0_return_0() {
+    public void input_0_plus_0_return_0() {
         test_calculate("0+0", 0);
     }
 
     @Test
-    public void test_input_0plus1_return_1() {
+    public void input_0_plus_1_return_1() {
         test_calculate("0+1", 1);
     }
 
     @Test
-    public void test_input_1plus0_return_1() {
+    public void input_1_plus_0_return_1() {
         test_calculate("1+0", 1);
     }
 
     @Test
-    public void test_input_1multiply1_return_1() {
+    public void input_1_multiply_1_return_1() {
         test_calculate("1*1", 1);
     }
 
