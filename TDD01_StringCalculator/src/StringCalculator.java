57982c5b0882bd5c32eaf60efcdb686aadd921a4
nikbin@itemis.de <nikbin@itemis.de>
2018-11-02 15:45:04 +0100

    REFACTOR - unify calculation logic for the + and * operators

--- TDD01_StringCalculator/src/StringCalculator.java
+++ TDD01_StringCalculator/src/StringCalculator.java
@@ -2,18 +2,22 @@
 public class StringCalculator {
 
     public int calculate(String s) {
+
         int indexOfPlus = s.indexOf("+");
         if (indexOfPlus != -1) {
             int firstOperand = Integer.parseInt(s.substring(0, indexOfPlus));
             int secondOperand = Integer.parseInt(s.substring(indexOfPlus));
             return firstOperand + secondOperand;
-        } else if (s.contains("*2"))
-            return 2;
-        else if (s.contains("*3"))
-            return 3;
-        else if (s.contains("*"))
-            return 1;
+        } else {
+            int indexOfStar = s.indexOf("*");
 
-        return Integer.parseInt(s);
+            if (indexOfStar != -1) {
+                int firstOperand = Integer.parseInt(s.substring(0, indexOfStar));
+                int secondOperand = Integer.parseInt(s.substring(indexOfStar + 1));
+                return firstOperand * secondOperand;
+            } else {
+                return Integer.parseInt(s);
+            }
+        }
     }
 }
