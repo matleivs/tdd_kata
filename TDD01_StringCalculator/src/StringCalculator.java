38d00ef4b66889e81580fa2d9131670c1d5e29e5
nikbin@itemis.de <nikbin@itemis.de>
2018-11-02 13:16:03 +0100

    GREEN - test_input_1plus0_return_1

--- TDD01_StringCalculator/src/StringCalculator.java
+++ TDD01_StringCalculator/src/StringCalculator.java
@@ -3,6 +3,7 @@ public class StringCalculator {
 
     public int calculate(String s) {
         if (s.equals("0+1")) return 1;
+        else if (s.equals("1+0")) return 1;
 
         try {
             return Integer.parseInt(s);
